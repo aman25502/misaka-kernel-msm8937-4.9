7cd36abe509e55a8e26eee3f3490126cd8c16191
aman25502 <65896145+aman25502@users.noreply.github.com>
2020-06-26 17:17:59 +0530

    Update unified.sh

--- unified.sh
+++ unified.sh
@@ -10,8 +10,7 @@ export TZ="Asia/Kolkata";
 # Kernel compiling script
 mkdir -p $HOME/TC
 git clone https://github.com/aman25502/AnyKernel3 -b santoni 
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r55 $HOME/TC/aarch64-linux-gnu-4.9 --depth=1
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 $HOME/TC/arm-linux-gnu-4.9 --depth=1  
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts --depth=1  
 
 # Upload log to del.dog
 function sendlog {
@@ -50,18 +49,18 @@ mkdir -p ${KERNELDIR}/aroma
 mkdir -p ${KERNELDIR}/files
 
 export KERNELNAME="Testkernelqbranchmisaka"
-export BUILD_CROSS_COMPILE="$HOME/TC/aarch64-linux-gnu-4.9/bin/aarch64-linux-gnu-"
-export CROSS_COMPILE_ARM32="$HOME/TC/arm-linux-gnu-4.9/bin/arm-linux-androideabi-"
+export CC=clang
+export BUILD_CROSS_COMPILE="$HOME/TC/aarch64-linux-gnu-8.x/bin/aarch64-linux-gnu-"
 export SRCDIR="${KERNELDIR}";
 export OUTDIR="${KERNELDIR}/out";
 export ANYKERNEL="${KERNELDIR}/AnyKernel3";
 export AROMA="${KERNELDIR}/aroma/";
 export ARCH="arm64";
 export SUBARCH="arm64";
-export KBUILD_COMPILER_STRING="$($KERNELDIR/prebuilts/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_COMPILER_STRING="$($KERNELDIR/prebuilts/clang-r365631c/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export KBUILD_BUILD_USER="AHOY"
 export KBUILD_BUILD_HOST="AHOYTESTKERNELS"
-export PATH="$KERNELDIR/prebuilts/proton-clang/bin:${PATH}"
+export PATH="$KERNELDIR/prebuilts/clang-r365631c/bin:${PATH}"
 export DEFCONFIG="santoni_treble_defconfig";
 export ZIP_DIR="${KERNELDIR}/files";
 export IMAGE="${OUTDIR}/arch/${ARCH}/boot/Image.gz-dtb";
@@ -117,7 +116,7 @@ echo -e "Using ${JOBS} threads to compile"
  
 # Start the build
 # ================
-${MAKE} -j${JOBS} \ ARCH=arm64 \ CROSS_COMPILE="$HOME/TC/aarch64-linux-gnu-4.9/bin/aarch64-linux-android-" \ CROSS_COMPILE_ARM32="$HOME/TC/arm-linux-gnu-4.9/bin/arm-linux-androideabi-" | tee build-log.txt ;
+${MAKE} -j${JOBS} \ ARCH=arm64 \ CC=clang \ CLANG_TRIPLE=aarch64-linux-gnu- \ CROSS_COMPILE="$HOME/TC/aarch64-linux-gnu-4.9/bin/aarch64-linux-android-" \ CROSS_COMPILE_ARM32="$HOME/TC/arm-linux-gnu-4.9/bin/arm-linux-androideabi-" \ NM=llvm-nm \ OBJCOPY=llvm-objcopy \ OBJDUMP=llvm-objdump \ STRIP=llvm-strip | tee build-log.txt ;
  
  
 exitCode="$?";
